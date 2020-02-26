f43a8d55eaca98b19705733fefdb73ecfc605c54
Praveen Yen <praveen.y@mistay.in>
2020-02-26 12:13:07 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 let express = require('express')
 let bodyParser = require('body-parser')
-let mongoose = require('mongoose')
+let cors = require('cors')
 
 let app = express()
+var allowedOrigins = ['http://localhost:3000', 'https://bazinga-dev.herokuapp.com'];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 let storeApiRoutes = require('./store/api-routes')
 
 var PORT = process.env.PORT || 8000
